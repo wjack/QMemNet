7ccd4fd607ffdaf0601a369c47e98ceeb85e97d2
Will Jack <wjack@mit.edu>
2015-11-18 01:30:40 -0500

    Began outlining of the model for a simple N2N memory network for language modelling, added example of a simple feedforward net in Tensorflow that I am using a acrude template. Plan to get this working, and add complexity/move to language modelling afterward.

---
+++ memory_net.py
@@ -0,0 +1,54 @@
+import math
+import tensorflow as tf
+
+#memory network for language modelling
+
+
+#reads previous N words into memory, individually
+#each memory cell holds a single word
+#temporal encoding is used
+#query vector is fixed to constant .1 without embedding.
+#cross entropy loss is used
+
+
+import tensorflow as tf
+import numpy as np
+import input_data
+
+
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+def model(A_embed_weights, B_embed_weights, C_embed_weights, W_output_weights, input_vectors, query):
+    #model performs k = 1 memory hops
+
+    #encode input vectors into m and c memory
+    m_memory = []
+    c_memory = []
+    for input_vector in input_vectors:
+
+        embedded_m_vector = tf.nn.relu(tf.matmul(input_vector, A_embed_weights), a_is_sparse = True)
+        embedded_c_vector  = tf.nn.relu(tf.matmul(input_vector, C_embed_weights), a_is_sparse = True)
+        m_memory.append(embedded_m_vector)
+        c_memory.append(embedded_c_vector)
+
+    m_memory = tf.convert_to_tensor(m_memory, dtype = tf.float32)
+    c_memory = tf.convert_to_tensor(c_memory, dtype = tf.float32)
+
+    #compute match between embedded memories in m_memory, and embedded query, p is a probability vector over the inputs.
+
+    embedded_query = tf.matmul(query, B_embed_weights)
+    memory_matches = tf.matmul(embedded_query, m_memory, transpose_a=True)
+
+    p = tf.softmax(memory_matches)
+
+    #compute output vector o by weighting c_memory by p
+
+    o = tf.matmul(p, c_memory, transpose_a = True)
+
+    #in a single memory hop, sum o with embedded_query, pass through output weights W, and then softmax
+
+    a = tf.softmax(tf.matmul(tf.add(o, embedded_query), W_output_weights))
+
+    #a is a probability distribution over the vocabulary
+    return a
